1c36e463bb26a5f859ae6af73268c2be689bad05
Roushan Singh <github.rtron18@gmail.com>
2024-12-19 21:23:40 +0530

    Updates

---
+++ precache-manifest.19e6c5384d7f1fc81c17cb2130e8e6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4211b50942815ffdec7f1045372b521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3509e71b572c7960ae",
+    "url": "/static/css/main.e22fe355.chunk.css"
+  },
+  {
+    "revision": "17ce46f434265d83d8ad",
+    "url": "/static/js/2.e951d4e8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.e951d4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3509e71b572c7960ae",
+    "url": "/static/js/main.cc92967a.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
